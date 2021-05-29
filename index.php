a71746169584f6db177854797ff9102781796903
kevin <kevin.vanbockryck@hotmail.com>
2021-05-29 10:41:15 +0200

    error messages

--- index.php
+++ index.php
@@ -1,29 +1,18 @@
 <?php
 
-    if(!empty($_POST)){
+$code = $_POST['code'];
+$success = "";
+$error = "";
 
-        //CONNECTION WITH DATABASE
-        $conn = new mysqli("localhost", "root", "root", "kluis");
-        $code = $_POST["code"];
-
-        $q = $conn->prepare("SELECT * FROM kluis WHERE code = :code");
-        $q->bindValue(":code", $code);
-        $q->execute();
-
-
-        $kluis = $q->fetch();
-
-        //CODE IS NOT CORRECT
-        if($kluis["code"] !== $code){
-            $error = "Code is niet correct!";
-
-        //CODE IS CORRECT
-        } else if ($kluis["code"] === $code){
-            $success = "Je hebt de kluis geopend!";
-        };
-
-
-    };
+if (isset($_POST['open'])) {
+    if ($code == "password") {
+        $success = "proficiat, zuip maar";
+        $error = "";
+    } else {
+        $error = "sorry, probeer nogeens";
+        $success = "";
+    }
+};
 
 ?>
 
@@ -51,18 +40,11 @@
 
             <form action="#" method="POST">
 
-            
-            <?php if(isset($error)): ?>
-                <div class="error"><?php echo $error; ?></div>
-            <?php endif; ?>
-
-            <?php if(isset($success)): ?>
-                <div class="success"><?php echo $success; ?></div>
-            <?php endif; ?>
+                <p><?php echo $error; ?><?php echo $success; ?></p>
 
                 <label>Vul de code in</label>
-                <input type="password" id="code" name="code" placeholder="Code">
-                <button type="submit" name="Open Locker" id="btnSubmit">CONTROLEER CODE</button>
+                <input type="password" id="code" name="code" placeholder="Voer de code in">
+                <button type="submit" name="open" id="btnSubmit">CONTROLEER CODE</button>
 
             </form>
 
